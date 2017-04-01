ada5914e6e027bcdbd76997959bc063aee88f61e
resul <resul@rsltgy>
2017-04-01 23:20:35 +0300

    Database files are added. Group, user and group_user, because they are connected to each other with N-N. Intrinsically a user can be a member of more than one group and a group can has more than one member

---
+++ db/group_members.sql
@@ -0,0 +1,20 @@
+﻿
+
+CREATE SEQUENCE public.group_members_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE public.group_members_id_seq
+  OWNER TO postgres;
+
+
+CREATE TABLE public."group_members"
+(
+  id bigint NOT NULL DEFAULT nextval('group_members_id_seq'::regclass),
+  group_id character varying  NOT NULL references groups(group_id),
+  user_id character varying  NOT NULL references users(user_id),
+  membership_date timestamp, 
+  primary key(membership_date)
+);
---
+++ db/users.sql
@@ -0,0 +1,23 @@
+﻿
+ DROP SEQUENCE public.group_id_seq;
+ drop table public.groups;
+
+CREATE SEQUENCE public.group_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE public.group_id_seq
+  OWNER TO postgres;
+
+
+CREATE TABLE public."groups"
+(
+  id bigint NOT NULL DEFAULT nextval('group_id_seq'::regclass),
+  group_id character varying  NOT NULL UNIQUE,
+  group_name character varying  NOT NULL,
+  creation_date timestamp,
+  creator_email character varying NOT NULL,
+  primary key(group_id)
+);
